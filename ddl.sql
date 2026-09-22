ce2f0fede905760f3487eab4b6ea751d29bdb9a7
leticiaguarizotolloto <leticiagtolloto@gmail.com>
2026-09-22 13:27:01 -0300

    up02

--- ddl.sql
+++ ddl.sql
@@ -11,19 +11,19 @@ create table usuario(
     nome varchar(100) not null,
     email varchar(100) not null unique,
     telefone varchar(15) unique,
-    departamento varchar(100),
-    cargo varchar (100),
-    status enum('ATIVO','BANIDO') not null
+    departamento varchar(50),
+    cargo varchar (50),
+    status enum('ATIVO','BANIDO')(20) not null
 );
 -- Criar a tabela de Chamado
 create table chamado(
     id int primary key not null auto_increment,
-    titulo varchar (100) not null,
+    titulo varchar (150) not null,
     descricao text not null,
     data_abertura datetime not null,
     data_fechamento datetime not null,
-    status enum('ABERTO','FECHADO') not null,
-    prioridade enum('BAIXA','MEDIA', 'ALTA', 'URGENTE'),
+    status enum('ABERTO','FECHADO')(30) not null,
+    prioridade enum('BAIXA','MEDIA', 'ALTA', 'URGENTE')(20),
     id_usuario int not null,
     id_categoria int not null, 
     id_tecnico int
@@ -34,13 +34,13 @@ create table tecnico(
     nome varchar (100) not null, 
     email varchar(100) not null unique,
     especialidade varchar(100), 
-    status enum('ATIVO','INATIVO') not null
+    status enum('ATIVO','INATIVO')(20) not null
 );
 --Criar tabela de Categoria
 create table categoria(
      id int primary key not null auto_increment,
-     nome varchar (100) not null,
-     descricao text
+     nome varchar (50) not null,
+     descricao text (255)
 );
 --Criar tabela de Histórico
 create table historico(
@@ -49,7 +49,7 @@ create table historico(
     id_usuario int not null,
     data_hora datetime not null, 
     descricao text,
-    tipo varchar (100) not null
+    tipo varchar (30) not null
 
 );
 -- Criar as chaves estrangeiras (Relacionamentos)
